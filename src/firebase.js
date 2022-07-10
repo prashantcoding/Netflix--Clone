7a24ac4d11c6eb6279d77472c674a6ae23e14912
prashantcoding <soultaker399@gmail.com>
2022-07-10 12:26:26 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA-An870z2R9CsdZ1Qsl7syxuoN6WNf4-8",
+    authDomain: "netflix-7aa30.firebaseapp.com",
+    projectId: "netflix-7aa30",
+    storageBucket: "netflix-7aa30.appspot.com",
+    messagingSenderId: "669360592469",
+    appId: "1:669360592469:web:d569cb960adc6a9fc326dc",
+    measurementId: "G-D6ERB3R20M"
+  };
